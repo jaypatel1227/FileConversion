998e015f7ef0a53bbb6e1b6cf0612a009d3a52f5
Jay Patel <jaypatel122700@gmail.com>
2024-04-21 03:21:26 -0500

    a lot more progress

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,8 +1,14 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-use serde::{Deserialize, Serialize};
 mod service;
+use actix_files::Files;
+use actix_multipart::Multipart;
+use actix_web::{get, post, web, App, Error, HttpResponse, HttpServer, Responder, Result};
+use futures::{StreamExt, TryStreamExt};
+use serde::{Deserialize, Serialize};
+use std::fs;
 use std::fs::File;
+use std::io::Write;
 use std::path::Path;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 #[derive(Debug, Deserialize, Serialize)]
 struct ServerConfig {
@@ -14,6 +20,12 @@ struct ServerConfig {
 struct AvialableService {
     name: String,
     service_func_name: String,
+}
+
+#[derive(Debug, Serialize)]
+struct ConversionResponse {
+    file_name: String,
+    success: bool,
 }
 
 fn get_server_config() -> ServerConfig {
@@ -33,7 +45,88 @@ async fn get_options() -> impl Responder {
         .insert_header(("Access-Control-Allow-Origin", "*")) // to stop CORS error when running the
         // site and service from the same domain (such as localhost)
         .json(get_server_config())
-    // Ok(web::Json(get_server_config().into()));
+}
+
+#[post("/convert_file/{conversion_type}/")]
+async fn convert_file(
+    path: web::Path<String>,
+    mut payload: Multipart,
+) -> Result<HttpResponse, Error> {
+    let mut filename = "".to_string();
+    let time_stamp = format!(
+        "{}",
+        SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_micros()
+    );
+    dbg!(time_stamp.clone());
+    dbg!(path.clone());
+
+    while let Ok(Some(mut field)) = payload.try_next().await {
+        let field_type = field.content_disposition().get_name().unwrap();
+        if field_type == "file" {
+            continue;
+        }
+        filename = format!(
+            "{}-{}",
+            time_stamp,
+            sanitize_filename::sanitize(field.content_disposition().get_filename().unwrap())
+        );
+
+        fs::create_dir_all("./input/")?;
+        let filepath = format!("{}{}", "./input/", sanitize_filename::sanitize(&filename));
+        dbg!(filepath.clone());
+        // File::create is blocking operation, use thread pool
+        let mut f = web::block(|| std::fs::File::create(filepath))
+            .await
+            .unwrap();
+
+        dbg!(field_type);
+        dbg!(filename.clone());
+
+        // Field in turn is stream of *Bytes* object
+        while let Some(chunk) = field.next().await {
+            let data = chunk.unwrap();
+            // filesystem operations are blocking, we have to use thread pool
+            f = web::block(move || {
+                f.as_mut()
+                    .expect("Error reading the file.")
+                    .write_all(&data)
+                    .map(|_| f)
+            })
+            .await?
+            .expect("Error when writing the data.");
+        }
+    }
+
+    let conversion_type = path.into_inner();
+    let matching_service = get_server_config()
+        .available_services
+        .into_iter()
+        .find(|s| s.name == conversion_type);
+
+    dbg!(conversion_type.clone());
+
+    match matching_service {
+        None => return Ok(HttpResponse::BadRequest().body("Invalid Conversion Request!")),
+        Some(service_info) => {
+            let converted_file = service::call(service_info.service_func_name, filename.clone());
+
+            // dbg!(service_info.service_func_name.clone());
+            dbg!(service_info.name.clone());
+            dbg!(converted_file.clone());
+
+            let resp = ConversionResponse {
+                file_name: converted_file,
+                success: true,
+            };
+
+            return Ok(HttpResponse::Ok()
+                .insert_header(("Access-Control-Allow-Origin", "*"))
+                .json(resp));
+        }
+    }
 }
 
 #[post("/echo")]
@@ -42,6 +135,11 @@ async fn echo(req_body: String) -> impl Responder {
         .insert_header(("Access-Control-Allow-Origin", "*"))
         .body(req_body)
 }
+
+// async fn index(req: HttpRequest) -> actix_web::Result<NamedFile> {
+//     let path: PathBuf = req.match_info().query("filename").parse().unwrap();
+//     Ok(NamedFile::open(path)?)
+// }
 
 async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
@@ -52,8 +150,11 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(get_options)
+            .service(convert_file)
+            .service(Files::new("/output", "./output").prefer_utf8(true)) // make the files under output accessible
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
+        // .route("/{filename:.*}", web::get().to(index))
     })
     .bind(("127.0.0.1", 5001))?
     .run()
--- app/src/service.rs
+++ app/src/service.rs
@@ -1,27 +1,24 @@
-mod service {
-    use once_cell::sync::Lazy;
-    use std::collections::HashMap;
+use once_cell::sync::Lazy;
+use std::collections::HashMap;
 
-    type ConversionFunction = fn(String) -> String; // this is the type signature of a conversion function
+type ConversionFunction = fn(String) -> String; // this is the type signature of a conversion function
 
-    // this is the mapping that is used to define the available conversions that are availiable
-    static AVAILABLE_CONVERSIONS: Lazy<HashMap<String, ConversionFunction>> = Lazy::new(|| {
-        let mut map = HashMap::new();
-        // Use a call like below to register a new conversion service
-        map.insert("docx_to_pdf".to_string(), docx_to_pdf as ConversionFunction);
-        map
-    });
+// this is the mapping that is used to define the available conversions that are availiable
+static AVAILABLE_CONVERSIONS: Lazy<HashMap<String, ConversionFunction>> = Lazy::new(|| {
+    let mut map = HashMap::new();
+    // Use a call like below to register a new conversion service
+    map.insert("docx_to_pdf".to_string(), docx_to_pdf as ConversionFunction);
+    map
+});
 
-    pub fn call(func_name: String, file_name: String) -> String {
-        if let Some(conversion_func) = AVAILABLE_CONVERSIONS.get(&func_name) {
-            let result = conversion_func(file_name);
-            println!("Conversion result: {}", result);
-            return result;
-        }
-        return "".to_string();
+pub fn call(func_name: String, file_name: String) -> String {
+    if let Some(conversion_func) = AVAILABLE_CONVERSIONS.get(&func_name) {
+        let result = conversion_func(file_name);
+        return result;
     }
+    return "".to_string();
+}
 
-    pub fn docx_to_pdf(file: String) -> String {
-        return "TODO".to_string();
-    }
+pub fn docx_to_pdf(file: String) -> String {
+    return file.replace(".docx", ".pdf").to_string();
 }
